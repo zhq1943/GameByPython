bcb4ab92fac8e9dc5ecd1d2b6b1760acac09ef6c
zhq1943 <engineer1943@163.com>
2017-02-23 20:41:01 +0800

    限制子弹数量

--- game_functions.py
+++ game_functions.py
@@ -13,8 +13,9 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
      elif event.key == pygame.K_DOWN:
          ship.moving_down = True 
      elif event.key == pygame.K_SPACE:
-         new_bullet = Bullet(ai_settings,screen,ship)
-         bullets.add(new_bullet)   
+         if len(bullets) < ai_settings.bullets_allowed:
+             new_bullet = Bullet(ai_settings,screen,ship)
+             bullets.add(new_bullet)   
 def check_keyup_events(event,ai_settings,screen,ship,bullets):
      if event.key == pygame.K_RIGHT:
          ship.moving_right = False
